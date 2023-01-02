e683b2705f6a15a3a9310014ee8cf17741f5efc7
dygdedesener <duygudede88@gmail.com>
2023-01-02 16:25:53 +0100

    adding explanation text to UI

--- server.R
+++ server.R
@@ -9,13 +9,13 @@ server = function(input, output,session) {
   
   # ################################################################
   # 
-  hideTab("tabs_trans", target = "filtering_trans")
-  hideTab("tabs_trans", target = "norm_trans")
-  hideTab("tabs_trans", target = "deg_trans")
-  hideTab("tabs_trans", target = "mapping_trans")
-  hideTab("tabs_trans", target = "pathway_trans")
-  hideTab("tabs_trans", target = "heatmap_trans")
-  hideTab("tabs_trans", target = "network_trans")
+  # hideTab("tabs_trans", target = "filtering_trans")
+  # hideTab("tabs_trans", target = "norm_trans")
+  # hideTab("tabs_trans", target = "deg_trans")
+  # hideTab("tabs_trans", target = "mapping_trans")
+  # hideTab("tabs_trans", target = "pathway_trans")
+  # hideTab("tabs_trans", target = "heatmap_trans")
+  # hideTab("tabs_trans", target = "network_trans")
   # 
   # ################################################################
   # 
@@ -23,7 +23,7 @@ server = function(input, output,session) {
   # 
   # ################################################################
   # 
-  hideTab("tabs_mets", target = "filtering_mets")
+  #hideTab("tabs_mets", target = "filtering_mets")
   #hideTab("tabs_mets", target = "norm_mets")
   #hideTab("tabs_mets", target = "stat_mets")
   #hideTab("tabs_mets", target = "mapping_mets")
